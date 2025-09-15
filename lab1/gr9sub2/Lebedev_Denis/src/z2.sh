518f8bca886b371230a1ec05177e3a03eba6b2f8
Dzianis Lebedzeu <3d.lebedzeu@gmail.com>
2025-09-15 19:44:39 +0300

    Lebedev Denis: lab1 done

---
+++ lab1/gr9sub2/Lebedev_Denis/src/z2.sh
@@ -0,0 +1,8 @@
+cat /var/log/auth.log \
+	| grep -iE 'Failed|Invalid' \
+	| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' \
+	| sort \
+	| uniq -c \
+	| sort -nr \
+	| head -n 10 \
+	| sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+/\1.x/'
---
+++ lab1/gr9sub2/Lebedev_Denis/src/z3.sh
@@ -0,0 +1,5 @@
+grep -oP '\s+install\s+\K\S+' /var/log/dpkg.log \
+	| sort \
+	| uniq -c \
+	| sort -nr \
+	| head -n 10
