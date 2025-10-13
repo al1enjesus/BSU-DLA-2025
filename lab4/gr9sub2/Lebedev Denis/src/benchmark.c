31fed4dfda8cfb8a14f6ecdaa4f87b90625a7e81
Denis Lebedev <3d.lebedzeu@gmail.com>
2025-10-13 12:22:17 +0300

    Task b

---
+++ lab4/gr9sub2/Lebedev Denis/src/benchmark.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <x86intrin.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+
+#define ITERATIONS 1000000
+#define CPU_FREQ_GHZ 3.3
+
+uint64_t measure_cycles(void (*func)()) {
+    uint64_t start, end;
+    start = __rdtsc();
+    for (int i = 0; i < ITERATIONS; i++) {
+        func();
+    }
+    end = __rdtsc();
+    return (end - start) / ITERATIONS;
+}
+
+__attribute__((noinline))
+int dummy() {
+    return 42;
+}
+
+void getpid_call() {
+    getpid();
+}
+
+void open_close_call() {
+    int fd = open("/home/testfile", O_WRONLY | O_CREAT, 0644);
+    char buf[4096];
+    read(fd, buf, sizeof(buf));
+    if (fd != -1) {
+        close(fd);
+    }
+}
+
+void gettimeofday_call() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+}
+
+int main() {
+    uint64_t dummy_cycles = measure_cycles((void (*)())dummy);
+    uint64_t getpid_cycles = measure_cycles(getpid_call);
+    uint64_t open_close_cycles = measure_cycles(open_close_call);
+    uint64_t gettimeofday_cycles = measure_cycles(gettimeofday_call);
+
+    double dummy_ns = (double)dummy_cycles / CPU_FREQ_GHZ;
+    double getpid_ns = (double)getpid_cycles / CPU_FREQ_GHZ;
+    double open_close_ns = (double)open_close_cycles / CPU_FREQ_GHZ;
+    double gettimeofday_ns = (double)gettimeofday_cycles / CPU_FREQ_GHZ;
+
+    printf("Benchmark results (1,000,000 iterations):\n");
+    printf("--------------------------------------------------------------------------------------\n");
+    printf("| Operation                 | Avg. Cycles | Avg. Time (ns) | Slower than userspace   |\n");
+    printf("--------------------------------------------------------------------------------------\n");
+    printf("| dummy() userspace         | %-11lu | %-14.2f | 1.00x                   |\n", dummy_cycles, dummy_ns);
+    printf("| getpid()                  | %-11lu | %-14.2f | %.2fx                  |\n", getpid_cycles, getpid_ns, (double)getpid_cycles / dummy_cycles);
+    printf("| open(\"file_name\")+close() | %-11lu | %-14.2f | %.2fx                |\n", open_close_cycles, open_close_ns, (double)open_close_cycles / dummy_cycles);
+    printf("| gettimeofday() vDSO       | %-11lu | %-14.2f | %.2fx                  |\n", gettimeofday_cycles, gettimeofday_ns, (double)gettimeofday_cycles / dummy_cycles);
+    printf("--------------------------------------------------------------------------------------\n");
+    printf("Note: CPU frequency is set to %.2f GHz. Please adjust if needed.\n", CPU_FREQ_GHZ);
+
+    return 0;
+}
